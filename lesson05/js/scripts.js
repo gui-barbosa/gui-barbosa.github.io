068fb3ad50517a3526581f97a74225e46d641798
gui-barbosa <gui_gb@live.com>
2021-05-22 22:40:26 -0600

    preston-5 page final

---
+++ lesson05/js/scripts.js
@@ -0,0 +1,36 @@
+window.addEventListener('load', () => {
+    const hambutton = document.querySelector('.ham');
+    const mainnav = document.querySelector('#navigation');
+
+    hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+    //To solve the mid resizing issue with responsive class on
+    window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+})
+
+window.addEventListener('load', (event) => {
+    const week = document.querySelector('#name-of-day');
+    var d = new Date();
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    week.textContent = weekday[d.getDay()];
+
+    const dayNum = document.getElementById('day');
+    dayNum.textContent = d.getDate();
+
+    var monthName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const monthNum = document.getElementById('month');
+    monthNum.textContent = monthName[d.getMonth()];
+
+    const year = document.getElementById('year');
+    year.textContent = d.getFullYear();
+
+    const cry = document.querySelector("#copyright-year");
+    cry.textContent = d.getFullYear();
+})
