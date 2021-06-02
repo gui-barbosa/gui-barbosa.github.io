9b39696fa5f729452cd515e11b45218bd8f330ec
gui-barbosa <gui_gb@live.com>
2021-06-02 10:57:05 -0600

    script fixed

--- lesson06/js/scripts.js
+++ lesson06/js/scripts.js
@@ -52,12 +52,9 @@ window.addEventListener('load', (event) => {
 /* Wind Chill Temperature.*/
 window.addEventListener('load', () => {
     const calWindChill = (temperature, speed) => {
-        return (temperature >= 50 && speed > 3) 
-        ?
+        return (temperature >= 50 && speed > 3) ?
             Math.round(
-                35.74 + (0.6215 * temperature) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * (temperature
-                * Math.pow(speed, 0.16)))
-        ,):
+                35.74 + (0.6215 * temperature) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * (temperature * Math.pow(speed, 0.16))), ) :
             "N/A";
     }
 
