29470a3b557beb1e3e289704c3a644ebf4d4b7d7
gui-barbosa <gui_gb@live.com>
2021-07-02 11:34:17 -0600

    Fixed the date footer not showing

---
+++ lesson11/WeatherSite/scripts/dates.js
@@ -0,0 +1,36 @@
+/* Date format example : Monday, 7 June 2021.*/
+window.addEventListener('load', (event) => {
+    const week = document.querySelector('#name-of-day');
+    var d = new Date();
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    week.textContent = weekday[d.getDay()];
+
+    /* Pancake event banner*/
+    const pancake_event = document.getElementById('pancake_event');
+    if (d.getDay() == 5) {
+
+        pancake_event.style.display = "block";
+    } else {
+        pancake_event.style.display = "none";
+    }
+
+    const dayNum = document.getElementById('day');
+    dayNum.textContent = d.getDate();
+
+    var monthName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const monthNum = document.getElementById('month');
+    monthNum.textContent = monthName[d.getMonth()];
+
+    const year = document.getElementById('year');
+    year.textContent = d.getFullYear();
+
+    const cry = document.querySelector("#copyright-year");
+    cry.textContent = d.getFullYear();
+})
